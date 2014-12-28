aa172b25e429205f2a13eb58cd44f6dbc6dbcabe
glington <jun@Jun-no-MacBook.local>
2014-12-28 16:32:21 +0900

    Created by name is set from users login name. Organization name is set from new project setting screen.

---
+++ appTemplate/appTemplate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  appTemplate
+//
+//  Created by Groby Lington on 2014/12/28.
+//  Copyright (c) 2014 systems-power. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ appTemplate/appTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  appTemplate
+//
+//  Created by Groby Lington on 2014/12/28.
+//  Copyright (c) 2014 systems-power. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
